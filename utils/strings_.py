8c7f7a24d513b0b3c94f21c15871e48cfe78e3e5
Mbonea Godwin <mjema86@gmail.com>
2021-05-13 16:13:54 +0300

    reduced score safe

--- utils/strings_.py
+++ utils/strings_.py
@@ -1,10 +1,3 @@
-import os
-books_api=os.env['books_api']
-channel_name = os.env['channel_name'].replace()
 books_api_image = "http://books.google.com/books/content?id={}&printsec=frontcover&img=1&zoom=1&source=gbs_api"
-books_api_call = f"https://www.googleapis.com/books/v1/volumes?q={}&key={books_api}"
-channel_message_link = f"https://t.me/{channel_name}/{}"
-
-
-
-AIzaSyBbphNVAq9wGTsSMQAqRmmKxvTgjllrQNA
+books_api_call = "https://www.googleapis.com/books/v1/volumes?q={}&key=AIzaSyBbphNVAq9wGTsSMQAqRmmKxvTgjllrQNA"
+channel_message_link = "https://t.me/testlib34/{}"
