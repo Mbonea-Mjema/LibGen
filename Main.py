476465ebe5a3c1205be7bd79d66e58ba739c1e1f
Mbonea Godwin <mjema86@gmail.com>
2021-05-16 19:21:12 +0300

    Fixed ref

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
 from pyrogram import Client
 import  os
-token = "1705162095:AAEnIkCdAwvI4WDqM9AthSgcOlW_fPc7yrA"
+
 books_bot = Client(
     bot_token=os.environ["BOT_TOKEN"],
-    api_id=os.environ["API_TOKEN"],
+    api_id=os.environ["API_ID"],
     api_hash=os.environ["API_HASH"],
     config_file="./Keys/config.ini"
 )
--- utils/strings_.py
+++ utils/strings_.py
@@ -1,4 +1,4 @@
 import os
 books_api_image = "http://books.google.com/books/content?id={}&printsec=frontcover&img=1&zoom=1&source=gbs_api"
 books_api_call = "https://www.googleapis.com/books/v1/volumes?q={}&key="+os.environ["books_api"]
-channel_message_link = "https://t.me/"+os.environ["telegram channel"]+"/{}"
+channel_message_link = "https://t.me/"+os.environ["TG_CHANNEL"]+"/{}"
