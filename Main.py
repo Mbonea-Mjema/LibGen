a188dcbcd4023efd3b71f882e78e3dd8565ada3c
Mbonea Godwin <mjema86@gmail.com>
2021-05-16 19:09:59 +0300

    open Source ready?

--- Main.py
+++ Main.py
@@ -1,7 +1,12 @@
 from pyrogram import Client
-
+import  os
 token = "1705162095:AAEnIkCdAwvI4WDqM9AthSgcOlW_fPc7yrA"
-search_bot = Client("pdf", config_file="./Keys/config.ini", bot_token=token)
+books_bot = Client(
+    bot_token=os.environ["BOT_TOKEN"],
+    api_id=os.environ["API_TOKEN"],
+    api_hash=os.environ["API_HASH"],
+    config_file="./Keys/config.ini"
+)
 
 if __name__ == "__main__":
-    search_bot.run()
+    books_bot.run()
--- utils/strings_.py
+++ utils/strings_.py
@@ -1,3 +1,4 @@
+import os
 books_api_image = "http://books.google.com/books/content?id={}&printsec=frontcover&img=1&zoom=1&source=gbs_api"
-books_api_call = "https://www.googleapis.com/books/v1/volumes?q={}&key=AIzaSyBbphNVAq9wGTsSMQAqRmmKxvTgjllrQNA"
-channel_message_link = "https://t.me/testlib34/{}"
+books_api_call = "https://www.googleapis.com/books/v1/volumes?q={}&key="+os.environ["books_api"]
+channel_message_link = "https://t.me/"+os.environ["telegram channel"]+"/{}"
