14a172d930185b250a07994326a1acf59253c0a6
Mbonea-Mjema <mjema86@gmail.com>
2021-07-31 20:52:17 +0300

    Update Scaper.py

--- utils/Scaper.py
+++ utils/Scaper.py
@@ -51,11 +51,11 @@ def search_book(metadata: Book):
     lib_search = LibgenSearch()
     logging.info(metadata)
     if metadata.Author:
-        filters =  {"Author": metadata.Author}
+        #filters =  {"Author": metadata.Author}
         filters = {"Language": "English"}
 
         results = lib_search.search_title_filtered(
-            f"{metadata.Title}", filters, exact_match=False
+            f"{metadata.Title} {metadata.Author}", filters, exact_match=False
         )
     else:
         results = lib_search.search_title(metadata.Title)
